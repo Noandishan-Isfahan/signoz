1cf56a2b6e9da24e838d41f11b39b2db7ffb36ec
Amir Abolhasani <amiravtar@gmail.com>
2025-11-03 16:07:19 +0330

    feat: Add nginx with tls

--- deploy/bundle.sh
+++ deploy/bundle.sh
@@ -7,9 +7,11 @@ set -Eeuo pipefail
 ROOT="$(git rev-parse --show-toplevel 2>/dev/null || pwd)"
 cd "$ROOT"
 COMPOSE="deploy/docker/docker-compose.yaml"
+COMPOSE_TLS="deploy/docker/docker-compose.tls.yaml"
 [[ -f "$COMPOSE" ]] || { echo "ERROR: $COMPOSE not found"; exit 1; }
+[[ -f "$COMPOSE_TLS" ]] || { echo "ERROR: $COMPOSE_TLS not found"; exit 1; }
 # --- get unique image list (no pulling)
-mapfile -t IMAGES < <(docker compose -f "$COMPOSE" config --images | sort -u)
+mapfile -t IMAGES < <(docker compose -f "$COMPOSE" -f "$COMPOSE_TLS" config --images | sort -u)
 (( ${#IMAGES[@]} > 0 )) || { echo "ERROR: no images resolved from compose"; exit 2; }
 
 
@@ -33,7 +35,7 @@ fi
 for img in "${IMAGES[@]}"; do
     safe_name=$(echo "$img" | sed 's#[/:]#_#g').zst
     echo "Saving $img → images/$safe_name"
-    docker save "$img" | zstd -T10 -7 -v -o "${PKG_DIR}/images/$safe_name"
+    docker save "$img" | zstd -T0 -15 -v -o "${PKG_DIR}/images/$safe_name"
 done
 
 cp deploy/image_load.sh "${PKG_DIR}/images/"
@@ -42,8 +44,16 @@ cp -r deploy/common "${PKG_DIR}/"
 
 cp deploy/docker/otel-collector-config.yaml "${PKG_DIR}/docker/"
 cp deploy/docker/docker-compose.yaml "${PKG_DIR}/docker/docker-compose.yaml"
+cp deploy/docker/docker-compose.tls.yaml "${PKG_DIR}/docker/docker-compose.tls.yaml"
+cp deploy/docker/nginx.conf "${PKG_DIR}/docker/nginx.conf"
+
+read -p "Do you want to include certs? [y/N]: " COPY_CERTS
+if [[ "$COPY_CERTS" =~ ^[Yy]$ ]]; then
+    # Remove certs folder from the copied package if user said no
+    cp -r deploy/certs $PKG_DIR/certs
+fi
 
 ARCHIVE_NAME="dist/${VERSION_TAG}.tar"
 tar -cf $ARCHIVE_NAME -C $PKG_DIR .
-echo "Deploy package created: $ARCHIVE_NAME"
-echo "You can now transfer it to your production server."
+echo "Deploy package created: $(readlink -f $ARCHIVE_NAME)"
+echo "You can now transfer it to your production server"
